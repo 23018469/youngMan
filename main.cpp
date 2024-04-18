fb27aa5c91538e21810a1cb6dcc389e1c1e7ca6e
23018469 <127191947+23018469@users.noreply.github.com>
2024-04-18 19:35:41 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+char interest;
+         cin>>interest;
+
+         if(interest=='a'){
+            cout<<" young man chooses to persue a chubby lady"<<endl;
+         }
+         else if( interest=='b'){
+            cout<<" young man choosed to persue a slender lady"<<endl;
+         }
+         else if(interest=='c'){
+            cout<<" young man chooses to persue a yellow chubby lady"<<endl;
+         }
+         else if(interest=='d'){
+            cout<<"young man chooses to persue a yellow bone slender lady"<<endl;
+         }
+         else{
+            cout<<"nigga is gay"<<endl;
+         }
+
+    return 0;
+}
